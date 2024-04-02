d7452bbacc04487260d5a59f0439a8f998965e13
Chalisa Keaw Phoomsakha <44123218+chphoom@users.noreply.github.com>
2024-04-02 16:34:54 -0400

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,7 +22,7 @@ export default function Home() {
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between text-sm lg:flex">
+      <div className="z-10 w-full items-center justify-between text-sm lg:flex">
         <DndContext
           onDragEnd={({delta}) => {
             setCoordinates(({x, y}) => {
@@ -45,7 +45,10 @@ export default function Home() {
               <Tile 
               content={consonants[0]}/>
             </Draggable> */}
-            <Button label="skjdnvsndvosnovbsipnvpisnv" selected={selected} onClick={() => setSelected(!selected)}/>
+            <Button label="test" selected={selected} onClick={() => {
+              setSelected(!selected);
+              // console.log(selected);
+            }}/>
         </DndContext>
       </div>
     </main>
